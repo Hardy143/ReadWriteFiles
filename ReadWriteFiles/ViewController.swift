9f99e393e860b12c3f06cd9dffc78ea299f6536b
Hardy143 <vickihardy88@gmail.com>
2018-07-26 15:19:38 +0100

    Reading and writing txt files

--- ReadWriteFiles/ViewController.swift
+++ ReadWriteFiles/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var fileView: UITextView!
+    
+    let file = "file.txt"
+    let text = "This is a text file that I'm\n testing out to see how\n it is"
+    var fileUrl: URL?
+    
+    func createDirectory() {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            fileUrl = dir.appendingPathComponent(file)
+            // write to file
+            do {
+                try text.write(to: fileUrl!, atomically: false, encoding: .utf8)
+                print("file written")
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func readFile() -> String? {
+        
+        var text2 = ""
+        do {
+            text2 = try String(contentsOf: fileUrl!, encoding: .utf8)
+        } catch {
+            print(error)
+        }
+        return text2
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createDirectory()
+        fileView.text = readFile()
     }
 
     override func didReceiveMemoryWarning() {
